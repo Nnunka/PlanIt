2b9270409978dae7675500df8ccb6c524a9b12e6
Paulina Hudzik <paulinanunab@gmail.com>
2024-10-24 22:17:23 +0200

    logowanie poprawki

--- planit/index.js
+++ planit/index.js
@@ -1,39 +1,29 @@
 const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
-const db = require("./config/db");
-
 const app = express();
+const path = require("path");
+const db = require("./config/db");
 
 app.use(express.json());
 
-// Ustawienie EJS jako silnika widoków
+app.use(express.static(path.join(__dirname, "public")));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static(path.join(__dirname, "public")));
-
-// Trasa głowna do logowania
 app.get("/", (req, res) => {
   res.render("login");
 });
 
 app.post("/login", (req, res) => {
-  const { login, password } = req.body; // Odbieramy 'login' zamiast 'username'
+  const { login, password } = req.body;
 
-  // Logowanie danych, aby upewnić się, że są poprawnie odbierane
-  console.log("Login:", login);
-  console.log("Password:", password);
+  console.log("Odebrane dane:", req.body);
 
   if (!login || !password) {
-    return res
-      .status(400)
-      .json({ message: "Login and password are required." });
+    return res.json({ message: "Login and password are required." });
   }
 
-  // Zmienione zapytanie SQL zgodnie z nazwami kolumn w bazie danych
-  const query =
-    "SELECT * FROM users WHERE user_login = ? AND user_password = ?";
+  const query = `SELECT * FROM users WHERE user_login = ? AND user_password = ?`;
   db.execute(query, [login, password], (err, results) => {
     if (err) {
       return res.status(500).json({ message: "Database error." });
